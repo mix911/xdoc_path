7ef8f45df57ac24eb111913c4eda4f015a287120
mix911 <wmihanale@gmail.com>
2020-02-16 14:40:16 +0300

    xtiny_path: xtiny_path module was moved to th::xml namespace.

--- xdoc_path/xtiny_path.h
+++ xdoc_path/xtiny_path.h
@@ -4,5 +4,7 @@
 #include <vector>
 #include <string>
 
-std::vector<TiXmlBase*> request_by_xpath(const std::string& xpath, TiXmlNode* node);
-std::vector<const TiXmlBase*> request_by_xpath(const std::string& xpath, const TiXmlNode* node);
+namespace th { namespace xml {
+    std::vector<TiXmlBase*> request_by_xpath(const std::string& xpath, TiXmlNode* node);
+    std::vector<const TiXmlBase*> request_by_xpath(const std::string& xpath, const TiXmlNode* node);
+} }
--- xpath/test_request_by_xpath.cpp
+++ xpath/test_request_by_xpath.cpp
@@ -2,6 +2,7 @@
 #include <gtest/gtest.h>
 
 using namespace std;
+using namespace th::xml;
 
 TEST(TestRequestByXPathSuit, TestRequestByXPath)
 {
